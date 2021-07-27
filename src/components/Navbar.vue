149103fbd4c94ab378dd146bdb28a9c06f4eac53
Ravishan Balasooriya <ravimuay@gmail.com>
2021-07-27 19:15:27 +0530

    Added navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+        <v-app-bar color="blue" dark app>
+            <v-app-bar-nav-icon>
+
+
+            </v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase">
+                <span></span>
+
+            </v-toolbar-title>
+        </v-app-bar>
+    </nav>
+</template>
