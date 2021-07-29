fcae56c30787411e30495a044d18c12dbcf83a15
Ravishan Balasooriya <ravimuay@gmail.com>
2021-07-29 15:54:28 +0530

    Added Popup and Footer components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,9 @@
 <template>
     <v-footer dark padless app inset>
-        <v-card>
-            
-
+        <v-card class="flex" flat tile color="reed" :elevation="10">
+            <v-card-text class="py-2 white--text text-center">
+                {{new Date().getFullYear()}} - <strong> TAJIK Ideapro</strong>
+            </v-card-text>
         </v-card>
     </v-footer>
 </template>
