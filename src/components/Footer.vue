8635604d35a6c593457da6bafe2b127b3c942542
Ravishan Balasooriya <ravimuay@gmail.com>
2021-07-29 13:16:25 +0530

    Added Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-footer dark padless app inset>
+        <v-card>
+            
+
+        </v-card>
+    </v-footer>
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,9 +29,23 @@
              </v-btn>
        </v-app-bar>
        <v-navigation-drawer v-model="drawer" dark app class="blue darken-4">
+           <v-layout column align-center>
+               <v-flex class="mt-5">
+                   <v-avatar size="100">
+                       <v-img src="/user.png"></v-img>
+                   </v-avatar>
+                   <p class="white---text subheading mt-1 text-center">Username</p>
+               </v-flex>
+           </v-layout>
            <v-list flat>
                <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
+                   <v-list-item-action>
+                       <v-icon>{{link.icon}}</v-icon>
+                   </v-list-item-action>
+                   <v-list-item-content>
+                       <v-list-item-title>{{link.text}}</v-list-item-title>
 
+                   </v-list-item-content>
                </v-list-item>
            </v-list>
        </v-navigation-drawer>
