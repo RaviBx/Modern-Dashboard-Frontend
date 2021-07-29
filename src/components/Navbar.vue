117b7081b95243926c6764fb7d28b409929667c6
Ravishan Balasooriya <ravimuay@gmail.com>
2021-07-29 12:32:47 +0530

    Done Navbar changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,63 @@
 <template>
-    <nav>
-        <v-app-bar color="blue" dark app>
-            <v-app-bar-nav-icon>
-
-
-            </v-app-bar-nav-icon>
-            <v-toolbar-title class="text-uppercase">
-                <span class="font-weight-light">AAE</span>
-                <span> IdeaPro </span>
-
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-vtn text>
+   <nav>
+       <v-app-bar  color="blue" dark app >
+           <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+           <v-toolbar-title class="text-uppercase ">
+               <span class="font-weight-light">Tajik</span>
+               <span>IdeaPro</span>
+           </v-toolbar-title>
+           <v-spacer></v-spacer>
+           <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          v-on="on"
+        >
+          <v-icon left>expand_more</v-icon>
+            <span>Menu</span>
+        </v-btn>
+      </template>
+      <v-list flat>
+        <v-list-item v-for="link in links"  :key="link.text" router :to="link.route" active-class="border">
+          <v-list-item-title >{{link.text}}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+            </v-menu>
+            <v-btn text>
                 <span>Exit</span>
-                <v-icon right> exit_to_app </v-icon>
+                <v-icon right>exit_to_app</v-icon>
+             </v-btn>
+       </v-app-bar>
+       <v-navigation-drawer v-model="drawer" dark app class="blue darken-4">
+           <v-list flat>
+               <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
 
-            </v-vtn>
-        </v-app-bar>
-    </nav>
+               </v-list-item>
+           </v-list>
+       </v-navigation-drawer>
+   </nav>
 </template>
+
+<script>
+export default {
+    data: () => ({
+
+        drawer: true,
+        links: [
+
+            {icon: 'dashboard', text: 'Dashboard', route: '/'},
+            {icon: 'folder', text: 'My Projects', route: '/projects'},
+            {icon: 'person', text: 'Team', route: '/team'}
+        ]
+    })
+}
+</script>
+
+
+<style scoped>
+
+    .border{
+        border-left: 4px solid green;
+    }
+
+</style>
